cbe856fe0e7b7e28da3b6862476c2943fbfeedc8
whitejh <56130599+whitejh@users.noreply.github.com>
2024-08-20 15:42:40 +0900

    Grafana에서 Slack으로 Alert 보내기 & 애플리케이션 로그 모니터링 실습

---
+++ Monitoring/com.monitor.sample/src/main/java/com/monitor/sample/SampleController.java
@@ -0,0 +1,22 @@
+package com.monitor.sample;
+
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+
+@RestController
+public class SampleController {
+
+    private static final Logger logger = LoggerFactory.getLogger(SampleController.class);
+
+    @GetMapping("/")
+    public String hello(HttpServletResponse response) throws IOException {
+        logger.info("Attempted access to / endpoint resulted in 403 Forbidden");
+        response.sendError(HttpServletResponse.SC_FORBIDDEN, "Access Denied");
+        return null;
+    }
+}
